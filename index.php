091295b88d44c419ba08915eacc65887a5637ea7
wilsonngo <wilson.ngo@unimelb.edu.au>
2014-11-21 16:19:14 +1100

    added mgse style sheet

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
     foreach ($files as $file):
         $filename = preg_replace("/\.html$/i", "", $file);
         $title = preg_replace("/\-/i", " ", $filename);
-        $documentation = 'doc/'.$type.'/'.$file;
+        $documentation = '_doc/'.$type.'/'.$file;
         echo '<div class="sg-markup sg-section">';
         echo '<div class="sg-display">';
         echo '<h2 class="sg-h2"><a id="sg-'.$filename.'" class="sg-anchor">'.$title.'</a></h2>';
@@ -75,7 +75,11 @@
   <link rel="stylesheet" href="css/sg-style.css">
   
   <!-- Replace below stylesheet with your own stylesheet -->
-  <link rel="stylesheet" href="css/style.css">
+  <link media="all" href=" http://education.unimelb.edu.au/_mappdesign/mgsestaging_design2.css" type="text/css" rel="stylesheet">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="http://fast.fonts.net/jsapi/7e9801db-a894-431d-8544-b85ad9cf5f1e.js" type="text/javascript"></script>
 </head>
 <body>
     
@@ -106,25 +110,31 @@
     <div class="sg-about sg-section">
       <h2 class="sg-h2"><a id="sg-about" class="sg-anchor">About</a></h2>
       <p>Comments and documentation about your style guide. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus nobis enim labore facilis consequuntur! Veritatis neque est suscipit tenetur temporibus enim consequatur deserunt perferendis. Neque nemo iusto minima deserunt amet.</p>
-    </div><!--/.sg-about-->
+    </div>
+    <!--/.sg-about-->
     
     <div class="sg-colors sg-section">
       <h2 class="sg-h2"><a id="sg-colors" class="sg-anchor">Colors</a></h2>
-        <div class="sg-color sg-color--a"><span class="sg-color-swatch"><span class="sg-animated">#88ffda</span></span></div>
-        <div class="sg-color sg-color--b"><span class="sg-color-swatch"><span class="sg-animated">#4dd3c9</span></span></div>
-        <div class="sg-color sg-color--c"><span class="sg-color-swatch"><span class="sg-animated">#339db0</span></span></div>
-        <div class="sg-color sg-color--d"><span class="sg-color-swatch"><span class="sg-animated">#2078aa</span></span></div>
-        <div class="sg-color sg-color--e"><span class="sg-color-swatch"><span class="sg-animated">#3a517a</span></span></div>
-        <div class="sg-color sg-color--f"><span class="sg-color-swatch"><span class="sg-animated">#384355</span></span></div>
+        <div class="sg-color sg-color--a"><span class="sg-color-swatch"><span class="sg-animated">#ffffff</span></span></div>
+        <div class="sg-color sg-color--b"><span class="sg-color-swatch"><span class="sg-animated">#003366</span></span></div>
+        <div class="sg-color sg-color--c"><span class="sg-color-swatch"><span class="sg-animated">#005596</span></span></div>
+        <div class="sg-color sg-color--d"><span class="sg-color-swatch"><span class="sg-animated">#00457c</span></span></div>
+        <div class="sg-color sg-color--e"><span class="sg-color-swatch"><span class="sg-animated">#444444</span></span></div>
+        <div class="sg-color sg-color--f"><span class="sg-color-swatch"><span class="sg-animated">#badef5</span></span></div>
         <div class="sg-markup-controls"><a class="sg-btn--top" href="#top">Back to Top</a></div>
-    </div><!--/.sg-colors-->
+    </div>
+
+    <!--/.sg-colors-->
     
     <div class="sg-font-stacks sg-section">
       <h2 class="sg-h2"><a id="sg-fontStacks" class="sg-anchor">Font Stacks</a></h2>
-      <p class="sg-font sg-font-primary">"HelveticaNeue", "Helvetica", Arial, sans-serif;</p>
-      <p class="sg-font sg-font-secondary">Georgia, Times, "Times New Roman", serif;</p>
+      <p class="sg-font-primary"><p>'Univers W01', 'Helvetica Neue', Helvetica, Arial, sans-serif !default;</p>
+      <p class="sg-font-bold"><strong>'Univers W01', 'Helvetica', Arial, sans-serif;</strong></p>
+      <section class="sg-font-headings"><h3>'Univers W02',Helvetica,Arial,sans-serif;</h3></section>
       <div class="sg-markup-controls"><a class="sg-btn--top" href="#top">Back to Top</a></div>
     </div><!--/.sg-font-stacks-->
+
+
   </div><!--/.sg-info-->    
 
   <div class="sg-base-styles">    
@@ -133,13 +143,88 @@
   </div><!--/.sg-base-styles-->
 
   <div class="sg-pattern-styles">
-    <h1 class="sg-h1">Pattern Styles<small> - Design and mark-up patterns unique to your site.</small></h1>
+    <h1 class="sg-h1">Pattern Styles<small> - Design and mark-up patterns.</small></h1>
     <?php showMarkup('patterns'); ?>
     </div><!--/.sg-pattern-styles-->
   </div><!--/.sg-body-->
 
   <script src="js/sg-plugins.js"></script>
   <script src="js/sg-scripts.js"></script>
+<script src="http://education.unimelb.edu.au/__data/assets/js_file/0003/1159257/banner-roller.js"></script>
+<!--these scripts should be kept below the slider stuff or it will break-->
+<!--collapsible-->
+<script src="http://education.unimelb.edu.au/_design/js/jquery.collapsible.min.js"></script>
+<!--js expander-->
+<script src="http://education.unimelb.edu.au/__data/assets/js_file/0011/590906/jquery.expander.min.js"></script>
+<!--fitvid-->
+<script src="http://education.unimelb.edu.au/__data/assets/js_file/0009/1070388/fitVid.js"></script>
+
+<!--lazy sizes-->
+<script src="http://education.unimelb.edu.au/__data/assets/js_file/0010/1263277/lazysizes.min.js"></script>
+
+<script>
+jQuery(document).ready(function() {
+
+$("#main-content").fitVids();
+
+$('.abstract').expander({
+        slicePoint:       100,  // default is 100
+        expandPrefix:     '...', // default is '... '
+        detailClass: 'more-details',
+        summaryClass: 'summary',
+        expandText: 'read more',
+        expandEffect: 'show',
+        expandSpeed: 0,
+        collapseEffect: 'hide',
+        collapseSpeed: 0
+        });
+
+ var type = window.location.hash.substr(1);
+
+ $('.collapsible').collapsible({
+ defaultOpen: type,
+ speed: 'fast'
+ });
+
+//force scroll to opened accordion in Firefox
+ $('html,body').animate({
+  scrollTop: $('.collapse-open').offset().top 
+  },1000);
+
+});
+</script>
+
+<script>
+jQuery(document).ready(function() {
+
+$("#main-content").fitVids();
+
+$('.abstract').expander({
+        slicePoint:       100,  // default is 100
+        expandPrefix:     '...', // default is '... '
+        detailClass: 'more-details',
+        summaryClass: 'summary',
+        expandText: 'read more',
+        expandEffect: 'show',
+        expandSpeed: 0,
+        collapseEffect: 'hide',
+        collapseSpeed: 0
+        });
+
+ var type = window.location.hash.substr(1);
+
+ $('.collapsible').collapsible({
+ defaultOpen: type,
+ speed: 'fast'
+ });
+
+//force scroll to opened accordion in Firefox
+ $('html,body').animate({
+  scrollTop: $('.collapse-open').offset().top 
+  },1000);
+
+});
+</script>
 </body>
 </html>
  
